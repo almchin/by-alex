f60db2ba7e5300c7724271201c3505dc3c34fbfa
Alex Chin <72493400+almchin@users.noreply.github.com>
2024-09-04 02:24:44 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import vercel from '@sveltejs/adapter-vercel'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: vercel(),
+    target: '#svelte',
   },
-  preprocess: vitePreprocess()
+  preprocess: preprocess()
 };
 export default config;
